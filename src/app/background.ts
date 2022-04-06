480b1e86808de8c77581ea612222d343a645c53a
Szalay Balázs <szalay.balazs.ms@gmail.com>
2022-04-06 09:45:45 +0200

    added url change

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,4 +1,15 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("Background got a message!")
-    sendResponse({})
-})
+  console.log("Background got a message!");
+  sendResponse({});
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // read changeInfo data and do something with it
+  // like send the new url to contentscripts.js
+  if (changeInfo.url) {
+    chrome.tabs.sendMessage(tabId, {
+      message: "URL_CHANGE",
+      url: changeInfo.url,
+    });
+  }
+});
--- src/app/content.ts
+++ src/app/content.ts
@@ -1,8 +1,15 @@
 chrome.runtime.sendMessage({}, (response) => {
-    var checkReady = setInterval(() => {
-        if (document.readyState === "complete") {
-            clearInterval(checkReady)
-            console.log("We're in the injected content script!")
-        }
-    })
-})
+  var checkReady = setInterval(() => {
+    if (document.readyState === "complete") {
+      clearInterval(checkReady);
+      console.log("We're in the injected content script!");
+    }
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // listen for messages sent from background.js
+  if (request.message === "URL_CHANGE") {
+    console.log(request.url); // new url is now in content scripts!
+  }
+});
